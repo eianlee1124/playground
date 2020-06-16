83a9ffcbde986cb2b1485b64c4c0728c37cb5f50
eianlee1124 <eianlee1124@gmail.com>
2020-06-16 13:30:46 +0900

    add singly_linked_list.py

---
+++ datastructures/linkedlist/singly_linked_list.py
@@ -0,0 +1,43 @@
+try:
+    from _collections import deque
+except ImportError:
+    from collections import deque
+
+class Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return "Node(%s)" % self.data
+
+    __str__ = __repr__
+
+class SinglyLinkedList(object):
+
+    def __init__(self, nodes=None):
+        self.head = None
+        if nodes is not None:
+            nodes = deque(nodes)
+            node = Node(nodes.popleft())
+            self.head = node
+            for elem in nodes:
+                node.next = Node(elem)
+                node = node.next
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __repr__(self):
+        node = self.head
+        items = []
+        while node is not None:
+            items.append(str(node))
+            node = node.next
+        items.append("None")
+        return " -> ".join(items)
+
